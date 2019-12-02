0dc83dedd9dee39fa1593f08dc9c19ef068b3f64
cs1201 <cs1201@york.ac.uk>
2019-12-02 23:31:49 +0000

    add template

---
+++ Template.py
@@ -0,0 +1,20 @@
+# ADVENT OF CODE 2019 - Day 1
+import math
+
+# Open file and return lines as list
+with open("input.txt") as fp:
+    data = fp.readlines()
+
+
+def part_1():
+
+    return result
+
+def part_2():
+
+    return result
+
+
+if __name__ == "__main__":
+    print(f"Day2: pt.1 Result: {part_1()}")
+    print(f"Day2: pt.2 Result: {part_2()}")
